3e3b514e888f35df0c4e6dacfaf92c10a95c9113
Gia-Sherwin Ly <ly.giasherwin@gmail.com>
2023-11-10 21:21:03 -0500

    part C

---
+++ myscript.py
@@ -0,0 +1,12 @@
+import os
+
+badHash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+goodHash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+
+os.system(f'git bisect start {badHash} {goodHash}')
+
+commandTest = 'python manage.py test'
+
+os.system(f'git bisect run {commandTest}')
+
+os.system('git bisect reset')
