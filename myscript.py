bef0c07dac34d359e9a44350660c78513d5d14ec
Gia-Sherwin Ly <ly.giasherwin@gmail.com>
2023-11-14 20:36:50 -0500

    format script

--- myscript.py
+++ myscript.py
@@ -2,11 +2,8 @@ import os
 
 badHash = "c1a4be04b972b6c17db242fc37752ad517c29402"
 goodHash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+commandTest = "python manage.py test"
 
-os.system(f'git bisect start {badHash} {goodHash}')
-
-commandTest = 'python manage.py test'
-
-os.system(f'git bisect run {commandTest}')
-
-os.system('git bisect reset')
+os.system(f"git bisect start {badHash} {goodHash}")
+os.system(f"git bisect run {commandTest}")
+os.system("git bisect reset")
